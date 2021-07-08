0d7d71110e214ade8ada92a708b3412f8d43dd4b
Paras Gupta <guptaparas039@gmail.com>
2021-07-09 00:04:08 +0530

    Update import statement

--- playgame.py
+++ playgame.py
@@ -3,32 +3,37 @@ from PyInquirer import prompt, style_from_dict
 
 question = [
     {
-        'type': 'list',
-        'name': 'game',
-        'message': 'What would you like to play?',
-        'choices': [
-            'Dragon Realm',
-            'Rock Paper Scissor',
-            'Number Guess',
-            'Tic Tac Toe',
-            'Hangman'
-        ]
+        "type": "list",
+        "name": "game",
+        "message": "What would you like to play?",
+        "choices": [
+            "Dragon Realm",
+            "Rock Paper Scissor",
+            "Number Guess",
+            "Tic Tac Toe",
+            "Hangman",
+        ],
     }
 ]
 
-answer = prompt(question).get('game')
-if answer == 'Dragon Realm':
-    from dragon_realm import main
+answer = prompt(question).get("game")
+if answer == "Dragon Realm":
+    from src.dragon_realm import main
+
     main()
-elif answer == 'Rock Paper Scissor':
-    from RockPaperScissor import main
+elif answer == "Rock Paper Scissor":
+    from src.RockPaperScissor import main
+
     main()
-elif answer == 'Number Guess':
-    from guess_the_number import main
+elif answer == "Number Guess":
+    from src.guess_the_number import main
+
     main()
-elif answer == 'Tic Tac Toe':
-    from tictactoe import main
+elif answer == "Tic Tac Toe":
+    from src.tictactoe import main
+
     main()
 else:
-    from hangman import main
+    from src.hangman import main
+
     main()
